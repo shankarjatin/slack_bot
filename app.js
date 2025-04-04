bfd27a9cdf2fc195302a8886400524face11aa83
shankarjatin <shankarjatin1005@gmail.com>
2025-04-05 01:13:42 +0530

    changes

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  const rawBody = JSON.stringify(req.body);
+  req.rawBody = rawBody;
+  next();
+});
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
